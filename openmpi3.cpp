b2e8d13616428a2a27b3fad0efa1c7ce1b00108c
Ariel Maldonado <arielsebas2000@gmail.com>
2024-01-16 21:37:29 -0500

    Deber: Suma de forma generica:

--- openmpi3.cpp
+++ openmpi3.cpp
@@ -1,6 +1,23 @@
 #include <mpi.h>
 #include <stdio.h>  // Incluimos la biblioteca para printf
+#include <fstream>
 #include <string>
+#include <vector>
+#include <functional>
+#include <cmath>
+
+std::vector<int> read_file() {
+    std::fstream fs("./datos2.txt", std::ios::in);
+    std::string line;
+
+    std::vector<int> ret;
+
+    while (std::getline(fs, line)) {
+        ret.push_back(std::stoi(line));
+    }
+    fs.close();
+    return ret;
+}
 
 int sumar(int *tmp, int n) {
     int suma = 0;
@@ -10,7 +27,7 @@ int sumar(int *tmp, int n) {
     return suma;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
 
     MPI_Init(&argc, &argv);
 
@@ -19,42 +36,55 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-    int data[100];
-
     if (rank == 0) {
         std::printf("Total de ranks: %d\n", nprocs);
 
-        for (int i = 0; i < 100; i++) {
-            data[i] = i;
-        }
+        std::vector<int> datos = read_file();
+
+        int size = datos.size();
+        int count = std::ceil(size / nprocs);
+
+        std::printf("Size: %d, Count: %d\n", size, count);
+
+        int *data = datos.data();
 
         for (int rank_id = 1; rank_id < nprocs; rank_id++) {
-            std::printf("RANK_0 enviando datos a RANK_%d\n", rank_id);
-            int start = rank_id * 25;
-            MPI_Send(&data[start], 25, MPI_INT, rank_id, 0, MPI_COMM_WORLD);
+            int start = (rank_id - 1) * count;
+            MPI_Send(&data[start], count, MPI_INT, rank_id, 0, MPI_COMM_WORLD);
         }
 
-        int suma_ranks[4];
+        int suma_ranks[nprocs];
 
-        suma_ranks[0] = sumar(data, 25);
+        suma_ranks[0] = sumar(data + ((nprocs - 1) * count), size - ((nprocs - 1) * count));
+
+        std::printf("%d", suma_ranks[0]);
 
         for (int rank_id = 1; rank_id < nprocs; rank_id++) {
             MPI_Recv(&suma_ranks[rank_id], 1, MPI_INT, rank_id, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
 
-        std::printf("Sumas parciales: %d, %d, %d, %d\n", suma_ranks[0], suma_ranks[1], suma_ranks[2], suma_ranks[3]);
+        std::string str = "";
+        for (int i = 0; i < nprocs; i++) {
+            str = str + std::to_string(suma_ranks[i]) + ", ";
+        }
+        std::printf("Sumas parciales: %s\n", str.c_str());
 
         int sumaTotal = sumar(suma_ranks, nprocs);
 
         std::printf("Suma total: %d\n", sumaTotal);
+
     } else {
-        std::printf("RANK_%d recibiendo datos\n", rank);
+        MPI_Status status;
+        int count;
 
-        MPI_Recv(data, 25, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Probe(0, 0, MPI_COMM_WORLD, &status);
+        MPI_Get_count(&status, MPI_INT, &count);
 
-        int suma_parcial = sumar(data, 25);
+        int data[count];
 
-        std::printf("RANK_%d enviando suma parcial %d\n", rank, suma_parcial);
+        MPI_Recv(data, count, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        int suma_parcial = sumar(data, count);
 
         MPI_Send(&suma_parcial, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
